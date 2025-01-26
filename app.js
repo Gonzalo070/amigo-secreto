b1a1ef7a3c59930223a8105ea0e2b2e60725d940
Gonzalo Laspada <gonzalolaspada200@gmail.com>
2025-01-26 17:17:13 -0300

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("tu amigo secreto es....")
+//Declarando variables
+let listaAmigos = [];
+let amigos;
+
+//Función para añadir amigos a la lista
+function añadirAmigos() {
+    
+}
+
+//Función para limpiar la caja de texto
+function limpiarCaja() {
+    
+}
+
+//Función para actualizar la lista y mostrarla en pantalla
+function mostrarLista() {
+    
+}
+
+//Función para sortear un nombre aleatorio de la lista
+function sortearNombre() {
+    
+}
