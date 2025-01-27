7a3407a2ee9731d6ffa29812ec59d95b3c3fc9e6
Gonzalo Laspada <gonzalolaspada200@gmail.com>
2025-01-27 20:04:04 -0300

    actualiza el app.js

--- app.js
+++ app.js
@@ -4,16 +4,6 @@ let listaAmigos = [];
 // Función para agregar amigos a la lista
 function añadirAmigos() {
     let nuevoAmigo = document.getElementById('ingresarAmigo').value;
-    
-    // Verificar si la entrada no está vacía
-    if (nuevoAmigo.trim() !== '') {
-        listaAmigos.push(nuevoAmigo);
-        console.log(listaAmigos);
-        // Limpiar el campo de entrada
-        document.getElementById('ingresarAmigo').value = '';
-    } else {
-        console.log('Por favor ingresa un nombre válido.');
-    }
 }
 
 
